e153be703cca90aa5f2a0afd395f2ad4e0076618
AlssyLop <alcibiades.lopezf@gmail.com>
2026-06-05 10:04:25 -0500

    feat(aplicacion): agregar manejador y DTO de respuesta para consultar usuario

---
+++ src/main/java/com/plazoleta/usuarios/application/handle/ConsultarUsuarioHandle.java
@@ -0,0 +1,29 @@
+package com.plazoleta.usuarios.application.handle;
+
+import com.plazoleta.usuarios.application.dto.response.UsuarioConsultaResponse;
+import com.plazoleta.usuarios.dominio.api.ConsultarUsuarioPort;
+import com.plazoleta.usuarios.dominio.modelo.Usuario;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional(readOnly = true)
+public class ConsultarUsuarioHandle {
+
+    private final ConsultarUsuarioPort consultarUsuario;
+
+    public ConsultarUsuarioHandle(ConsultarUsuarioPort consultarUsuario) {
+        this.consultarUsuario = consultarUsuario;
+    }
+
+    public UsuarioConsultaResponse consultarPorId(Long id) {
+        Usuario usuario = consultarUsuario.consultarPorId(id);
+        return new UsuarioConsultaResponse(
+                usuario.getId(),
+                usuario.getNombre().getValor(),
+                usuario.getApellido().getValor(),
+                usuario.getCorreo().getValor(),
+                usuario.getRol().name()
+        );
+    }
+}
