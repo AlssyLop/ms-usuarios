096dfffed2f228dd2e34c8861c02e203696e3675
Alcibiades Lopez <alcibiades.lopezf@gmail.com>
2026-06-06 19:49:29 -0500

    Actualizar correo admin data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,2 @@
 INSERT IGNORE INTO usuario (nombre, apellido, documento_de_identidad, celular, fecha_nacimiento, correo, clave, id_rol, activo)
-VALUES ('Admin', 'Sistema', '0000000000', '+0000000000', '1990-01-01', 'admin', '$2a$10$m3ljp4PDCja6LvxwHqMl4e7/kXJJDh/S1s5PdzPTc58XtmAhz1rE6', 1, TRUE);
+VALUES (Admin', 'Sistema', '0000000000', '+0000000000', '1990-01-01', 'admin@admin.com', '$2a$10$m3ljp4PDCja6LvxwHqMl4e7/kXJJDh/S1s5PdzPTc58XtmAhz1rE6', 1, TRUE);
